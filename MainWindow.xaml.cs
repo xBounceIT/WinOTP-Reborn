382e613b7e9177f3016046aab52aeedfdae041ba
Daniel D'Angeli <dangelidaniel20@gmail.com>
2026-02-27 00:57:00 +0100

    Prevent add button spam and sync visibility with navigation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 using WinOTP.Pages;
 
 namespace WinOTP;
 
 public sealed partial class MainWindow : Window
 {
+    private bool _isAddFlowOpening;
+
     public MainWindow()
     {
         this.InitializeComponent();
@@ -12,11 +16,41 @@ public sealed partial class MainWindow : Window
         var appWindow = this.AppWindow;
         appWindow.Resize(new Windows.Graphics.SizeInt32(400, 600));
 
+        ContentFrame.Navigated += ContentFrame_Navigated;
         ContentFrame.Navigate(typeof(HomePage));
+    }
+
+    private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+    {
+        UpdateAddButtonState(e.SourcePageType);
+    }
+
+    private void UpdateAddButtonState(Type? pageType)
+    {
+        var isHomePage = pageType == typeof(HomePage);
+        AddButton.Visibility = isHomePage ? Visibility.Visible : Visibility.Collapsed;
+        AddButton.IsEnabled = isHomePage;
+
+        if (isHomePage)
+        {
+            _isAddFlowOpening = false;
+        }
     }
 
     private void AddButton_Click(object sender, RoutedEventArgs e)
     {
-        ContentFrame.Navigate(typeof(AddAccountPage));
+        if (_isAddFlowOpening)
+        {
+            return;
+        }
+
+        _isAddFlowOpening = true;
+        AddButton.IsEnabled = false;
+
+        if (!ContentFrame.Navigate(typeof(AddAccountPage)))
+        {
+            _isAddFlowOpening = false;
+            UpdateAddButtonState(ContentFrame.CurrentSourcePageType);
+        }
     }
 }
